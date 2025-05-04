1326a000c13b452e1f916e6e758299b17637b303
dbotana <dbotana0711@gmail.com>
2025-05-04 10:27:05 -0400

    update 1

--- Hengeyokai.js
+++ Hengeyokai.js
@@ -9,9 +9,9 @@ It is recommended to enter the code in a fresh sheet before adding any other inf
 Subject: Species
 Effect: This script adds the Hengeyokai races from the "Hengeyokai" homebrew source created by GreyFartBR https://www.reddit.com/user/GreyFartBR/
 Code by:  Rocky
-Date: 2025-05-03 (sheet v13)
+Date: 2025-05-04 (sheet v13)
 */
-var iFileName = "Sorcerer - Giant Soul Homebrew.js";
+var iFileName = "Hengeyokai.js";
 
 // Define the source
 SourceList["HGY"] = {
@@ -37,7 +37,6 @@ RaceList["hengeyokai"] = {
     languageProfs : ["Common", 1],
     age : " can live up to 1000 years",
     scoresGeneric : false,
-    scores : [0, 0, 0, 0, 0, 1], // +1 to Charisma
     trait : "Hengeyokai (+1 Charisma, and subrace bonuses)" +
         "\n \u2022 Nature Spirit: My creature type is fey, rather than humanoid." +
         "\n \u2022 Spirit Form: As an action, I can transform into my animal form. My game statistics remain the same except for characteristics listed in my subrace." +
@@ -52,7 +51,10 @@ RaceList["hengeyokai"] = {
     }],
     action : [["action", "Spirit Form (transform)"]]
 };
-
+var Hengeyokai_trait = (typePF ? "\n" : "") + " \u2022 Nature Spirit: My creature type is fey, rather than humanoid." +
+"\n \u2022 Spirit Form: As an action, I can transform into my animal form. My game statistics remain the same except for characteristics listed in my subrace." + 
+"\n \u2022 Animal Communication: I can speak with animals of the same type as my animal form in both forms." +
+"\n \u2022 Yokai Magic: I know one cantrip from the Sorcerer spell list. Charisma is my spellcasting ability for it.";
 // Carp Henge subrace
 AddRacialVariant("hengeyokai", "carp", {
     regExpSearch : /^(?=.*hengeyokai)(?=.*carp)|koi/i,
@@ -65,7 +67,7 @@ AddRacialVariant("hengeyokai", "carp", {
         walk : { spd : 30, enc : 20 },
         swim : { spd : "walk", enc : "walk" }
     },
-    scores : [0, 1, 1, 0, 0, 0], // +1 Dex, +1 Con
+    scores : [0, 1, 1, 0, 0, 1], // +1 Dex, +1 Con
     weaponOptions : [{
         baseWeapon : "unarmed strike",
         regExpSearch : /^(?=.*carp)(?=.*bite).*$/i,
@@ -75,8 +77,7 @@ AddRacialVariant("hengeyokai", "carp", {
         damage : [1, "", "piercing"],
         description : "Only usable in carp form"
     }],
-    trait : "Carp Hengeyokai (+1 Dexterity, +1 Constitution, +1 Charisma)" +
-        "\n \u2022 Perseverance: I have proficiency in Athletics and Survival." +
+    trait : "Carp Hengeyokai (+1 Dexterity, +1 Constitution, +1 Charisma)" + Hengeyokai_trait +
         "\n \u2022 Carp Spirit: When I use Spirit Form, I transform into a carp. As a carp, my size is Tiny, my base walking speed is 0, I have a swim speed of 40 feet, and my unarmed strikes are bites. I can only breathe underwater in carp form." +
         "\n \u2022 Amphibious: In humanoid form, I can breathe air and water."
 });
@@ -93,8 +94,8 @@ AddRacialVariant("hengeyokai", "cat", {
         walk : { spd : 30, enc : 20 },
         climb : { spd : 30, enc : 20 }
     },
-    scores : [0, 2, 0, 0, 0, 0], // +2 Dex
-    vision : [["Keen Smell", 0]],
+    scores : [0, 2, 0, 0, 0, 1], // +2 Dex
+    vision : [["Keen Smell", 0], ["Darkvision", 60]],
     weaponOptions : [{
         baseWeapon : "unarmed strike",
         regExpSearch : /^(?=.*cat)(?=.*claws).*$/i,
@@ -104,8 +105,7 @@ AddRacialVariant("hengeyokai", "cat", {
         damage : [1, "", "slashing"],
         description : "Finesse; Only usable in cat form"
     }],
-    trait : "Cat Hengeyokai (+2 Dexterity, +1 Charisma)" +
-        "\n \u2022 Sneaky and Flexible: I have proficiency in Acrobatics and Stealth." +
+    trait : "Cat Hengeyokai (+2 Dexterity, +1 Charisma)" + Hengeyokai_trait +
         "\n \u2022 Cat Spirit: When I use Spirit Form, I transform into a cat. As a cat, my size is Tiny, my base speed becomes 40 feet, and my unarmed strikes are claws dealing slashing damage." +
         "\n \u2022 Keen Smell: I have advantage on Wisdom (Perception) checks that rely on smell." +
         "\n \u2022 Climber: I have a climbing speed of 30 feet in both forms."
@@ -120,8 +120,8 @@ AddRacialVariant("hengeyokai", "dog", {
     plural : "Dog Hengeyokai",
     skills : ["Athletics", "Insight"],
     savetxt : { adv_vs : ["frightened", "prone"] },
-    scores : [2, 0, 0, 0, 0, 0], // +2 Str
-    vision : [["Keen Smell and Hearing", 0]],
+    scores : [2, 0, 0, 0, 0, 1], // +2 Str
+    vision : [["Keen Smell and Hearing", 0], ["Darkvision", 60]],
     weaponOptions : [{
         baseWeapon : "unarmed strike",
         regExpSearch : /^(?=.*dog)(?=.*bite).*$/i,
@@ -131,8 +131,7 @@ AddRacialVariant("hengeyokai", "dog", {
         damage : [1, 4, "piercing"],
         description : "Only usable in dog form"
     }],
-    trait : "Dog Hengeyokai (+2 Strength, +1 Charisma)" +
-        "\n \u2022 Man's Best Friend: I have proficiency in Athletics and Insight." +
+    trait : "Dog Hengeyokai (+2 Strength, +1 Charisma)" + Hengeyokai_trait +
         "\n \u2022 Dog Spirit: When I use Spirit Form, I transform into a dog. As a dog, my size is Small, my base speed becomes 40 feet, and my unarmed strikes are bites dealing 1d4 + my Strength modifier piercing damage." +
         "\n \u2022 Keen Smell and Hearing: I have advantage on Wisdom (Perception) checks that rely on smell or hearing." +
         "\n \u2022 Steadfast and Loyal: I have advantage on saving throws against being frightened or knocked prone."
@@ -145,8 +144,8 @@ AddRacialVariant("hengeyokai", "fox", {
     sortname : "Hengeyokai, Fox",
     source : [["HGY", 0]],
     plural : "Fox Hengeyokai",
-    skills : ["Deception", "Persuasion"],
-    scores : [0, 1, 0, 0, 0, 1], // +1 Dex, +1 Cha (total +2 Cha with base)
+    skills : [["Deception", "full"], "Persuasion"],
+    scores : [0, 1, 0, 0, 0, 2], // +1 Dex, +1 Cha (total +2 Cha with base)
     vision : [["Keen Smell and Hearing", 0]],
     weaponOptions : [{
         baseWeapon : "unarmed strike",
@@ -157,20 +156,9 @@ AddRacialVariant("hengeyokai", "fox", {
         damage : [1, 4, "piercing"],
         description : "Only usable in fox form"
     }],
-    trait : "Fox Hengeyokai (+1 Dexterity, +2 Charisma total)" +
-        "\n \u2022 Trickster: I have proficiency in Deception and Persuasion skills. I also double my proficiency bonus in Charisma (Deception) checks." +
+    trait : "Fox Hengeyokai (+1 Dexterity, +2 Charisma total)" + Hengeyokai_trait +
         "\n \u2022 Fox Spirit: When I use Spirit Form, I transform into a fox. As a fox, my size is Tiny, my base speed becomes 40 feet, and my unarmed strikes are bites dealing 1d4 + my Strength modifier piercing damage." +
         "\n \u2022 Keen Smell and Hearing: I have advantage on Wisdom (Perception) checks that rely on smell or hearing.",
-    calcChanges : {
-        skillAdd : [
-            function(skillObj) {
-                if (skillObj.name === "Deception") {
-                    skillObj.expert = true;
-                }
-            },
-            "I double my proficiency bonus for Deception checks."
-        ]
-    }
 });
 
 // Raccoon Dog Henge subrace
@@ -181,12 +169,12 @@ AddRacialVariant("hengeyokai", "raccoon dog", {
     source : [["HGY", 0]],
     plural : "Raccoon Dog Hengeyokai",
     skills : ["Insight", "Persuasion"],
-    scores : [0, 0, 1, 0, 0, 1], // +1 Con, +1 Cha (total +2 Cha with base)
+    scores : [0, 0, 1, 0, 0, 2], // +1 Con, +1 Cha (total +2 Cha with base)
     speed : {
         walk : { spd : 30, enc : 20 },
         climb : { spd : 30, enc : 20 }
     },
-    vision : [["Keen Smell", 0]],
+    vision : [["Keen Smell", 0], ["Darkvision", 60]],
     weaponOptions : [{
         baseWeapon : "unarmed strike",
         regExpSearch : /^(?=.*raccoon)(?=.*claws).*$/i,
@@ -196,16 +184,11 @@ AddRacialVariant("hengeyokai", "raccoon dog", {
         damage : [1, 4, "slashing"],
         description : "Finesse; Only usable in raccoon dog form"
     }],
-    trait : "Raccoon Dog Hengeyokai (+1 Constitution, +2 Charisma total)" +
-        "\n \u2022 Charm of a Merchant: I have proficiency in Insight and Persuasion skills. I also have advantage on Charisma (Persuasion) and Charisma (Deception) checks regarding money." +
+    trait : "Raccoon Dog Hengeyokai (+1 Constitution, +2 Charisma total)" + Hengeyokai_trait +
+        "\n \u2022 Charm of a Merchant: I have advantage on Charisma (Persuasion) and Charisma (Deception) checks regarding money." +
         "\n \u2022 Raccoon Dog Spirit: When I use Spirit Form, I transform into a raccoon dog. As a raccoon dog, my size is Tiny, and my unarmed strikes are claws dealing 1d4 + my Dexterity modifier slashing damage." +
-        "\n \u2022 Keen Smell: I have advantage on Wisdom (Perception) checks that rely on smell." +
-        "\n \u2022 Climber: I have a climbing speed of 30 feet in both forms.",
-    advantages : [
-        ["Persuasion", true, "if regarding money"],
-        ["Deception", true, "if regarding money"]
-    ]
-});
+        "\n \u2022 Keen Smell: I have advantage on Wisdom (Perception) checks that rely on smell."
+    });
 
 // Sparrow Henge subrace
 AddRacialVariant("hengeyokai", "sparrow", {
@@ -215,7 +198,7 @@ AddRacialVariant("hengeyokai", "sparrow", {
     source : [["HGY", 0]],
     plural : "Sparrow Hengeyokai",
     skills : ["Acrobatics", "Insight"],
-    scores : [0, 1, 0, 0, 1, 0], // +1 Dex, +1 Wis
+    scores : [0, 1, 0, 0, 1, 1], // +1 Dex, +1 Wis
     weaponOptions : [{
         baseWeapon : "unarmed strike",
         regExpSearch : /^(?=.*sparrow)(?=.*beak).*$/i,
@@ -225,8 +208,7 @@ AddRacialVariant("hengeyokai", "sparrow", {
         damage : [1, "", "piercing"],
         description : "Only usable in sparrow form"
     }],
-    trait : "Sparrow Hengeyokai (+1 Dexterity, +1 Wisdom, +1 Charisma)" +
-        "\n \u2022 Friend of the Honest: I have proficiency in Acrobatics and Insight skills." +
+    trait : "Sparrow Hengeyokai (+1 Dexterity, +1 Wisdom, +1 Charisma)" + Hengeyokai_trait +
         "\n \u2022 Sparrow Spirit: When I use Spirit Form, I transform into a sparrow. As a sparrow, my size is Tiny, my base speed becomes 10 feet, I have a fly speed of 50 feet, and my unarmed strikes are my beak.",
     features : {
         "sparrow form" : {
